db4654ff2a0256f23a7018124d46e5a8cacec07b
Eugene Teo <eugeneteo@gmail.com>
2014-11-12 10:43:28 +0800

    s/destfile/dataset/

--- datadive101/ufo_sightings.R
+++ datadive101/ufo_sightings.R
@@ -20,7 +20,7 @@ if (length(new.pkg)) {
 library(dplyr)
 
 dataset <- "./ufo_awesome.tsv"
-if (! file.exists(destfile)) {
+if (! file.exists(dataset)) {
   # shasum: 39cfc99c9814d8806526a1adeaf3c582e47b0f27
   file.url <- "https://raw.githubusercontent.com/johnmyleswhite/ML_for_Hackers/master/01-Introduction/data/ufo/ufo_awesome.tsv"
   download.file(file.url, destfile = dataset, method = "curl")
